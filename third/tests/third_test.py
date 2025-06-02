ca8a43f1a03ed29e65ea39165c16b5a765e576df
Xiaoli Zhang <81294502+xiaoliz0@users.noreply.github.com>
2025-06-02 16:32:37 +0000

    test3: add a function in the third_test.py

--- third/tests/third_test.py
+++ third/tests/third_test.py
@@ -1,5 +1,6 @@
 from third.third import greeting
 import pytest
+from contextlib import nullcontext
 
 def test_greeting_eng():
     assert greeting("James", "English") == "Hello James!"
@@ -28,3 +29,14 @@ def test_greeting_language_wrong_type():
 # remove the old test after you have completed the task
 # add your changes: git add third/tests/third_test.py
 # commit your changes using commit message conventions (https://inpred.github.io/24-03_bioinfo_ws/#19): git commit -m "test: <your commit message>"
+@pytest.mark.parametrize(
+    "name, language, exception, want",
+    [
+        ("xiaoli", "English", nullcontext(), "Hello xiaoli!"),
+        (1, "English", pytest.raises(TypeError), None)
+    ]
+)
+
+def test_greeting(name, language, exception, want):
+    with exception:
+        assert greeting(name, language) == want
